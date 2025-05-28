a906ae75aecfcaed6f949a7d12eb60e62bb5fe95
Utsav Lamichhane <132714550+utsavlamichhane@users.noreply.github.com>
2025-05-28 11:57:23 -0400

    Create Preprocessing_ASV.py

---
+++ Preprocessing_ASV.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+
+asv_df = pd.read_csv('ASV_table_clean.csv')
+
+#data dimension 73000 rows and  1168 columns (before transp)
+asv_transposed = asv_df.set_index('ASV ID').T
+
+
+asv_transposed = (
+    asv_transposed
+    .reset_index()
+    .rename(columns={'index': 'SampleID'})
+)
+
+##data dimesion 1168 rows and 2 cols
+rfi_df = pd.read_excel('overall_RFI_only.xlsx')
+
+
+
+##merging by SampleID
+merged = pd.merge(asv_transposed,
+                  rfi_df,
+                  on='SampleID',
+                  how='left')
+
+
+merged.to_csv('preprocessed_ASV_level_1.csv', index=False)
+
+print("Saved as 'preprocessed_ASV_level_1.csv'")
